9b2132296797053e9b4ea0840247eb33745b3a69
Anis Jonischkeit <anis.jonischkeit@gmail.com>
2017-11-15 20:32:53 +1000

    added shell script to pull from git and restart the server

--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
+const childProcess = require('child_process');
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/git-pull-update', (req, res) => {
+	childProcess.exec('sh ./pullFromGit.sh');
+	process.exit()
+})
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
