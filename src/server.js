ac11ac1474c042aa74e462b57deb2c1de91c6e47
Anis Jonischkeit <anis.jonischkeit@gmail.com>
2017-11-15 21:12:07 +1000

    saving output of pullfromgit to file

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const childProcess = require('child_process');
 app.get('/', (req, res) => res.send('Hello World! - This should have updated WOW4!!!!'))
 
 app.post('/git-pull-update', (req, res) => {
-	childProcess.exec('sh ./pullFromGit.sh');
+	childProcess.exec('sh ./pullFromGit.sh > out.txt');
 	res.send('restarting...')
 	process.exit()
 })
