c4bead68d9b5f2cf29b92842f417cb685fb8a20c
Anis Jonischkeit <anis.jonischkeit@gmail.com>
2017-11-15 20:57:57 +1000

    added restarting response to git-pull-update

--- src/server.js
+++ src/server.js
@@ -2,11 +2,12 @@ const express = require('express')
 const app = express()
 const childProcess = require('child_process');
 
-app.get('/', (req, res) => res.send('Hello World! - This should have updated WOW!!!!'))
+app.get('/', (req, res) => res.send('Hello World! - This should have updated WOW1!!!!'))
 
 app.post('/git-pull-update', (req, res) => {
 	childProcess.exec('sh ./pullFromGit.sh');
+	res.send('restarting...')
 	process.exit()
 })
 
-app.listen(8001, () => console.log('Example app listening on port 3000!'))
+app.listen(8001, () => console.log('Example app listening on port 8001!'))
