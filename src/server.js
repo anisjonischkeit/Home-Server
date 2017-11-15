6f61c459e34166609c1af1e4fd46eb0b7d2e7db7
Anis Jonischkeit <anis.jonischkeit@gmail.com>
2017-11-15 21:00:26 +1000

    trying again to update data on from github push

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const childProcess = require('child_process');
 
-app.get('/', (req, res) => res.send('Hello World! - This should have updated WOW1!!!!'))
+app.get('/', (req, res) => res.send('Hello World! - This should have updated WOW2!!!!'))
 
 app.post('/git-pull-update', (req, res) => {
 	childProcess.exec('sh ./pullFromGit.sh');
